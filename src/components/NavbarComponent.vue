d0d362291b48c43ffa307b9caecbb54b19522f08
Ray Unabia <unabiaray2@gmail.com>
2026-06-16 23:02:45 +0800

    Fix navbar component formatting and improve toggler styles

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -18,7 +18,7 @@ const { user } = storeToRefs(store);
 				<span class="navbar-toggler-icon"></span>
 			</button>
 
-			<div class="collapse navbar-collapse" id="navbarNavAltMarkup"> 
+			<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
 				<div class="navbar-nav ms-auto">
 
 					<div v-if="user?.isAdmin" class="admin-badge">
@@ -61,6 +61,14 @@ const { user } = storeToRefs(store);
 </template>
 
 <style scoped>
+.navbar-toggler {
+	border-color: rgba(255, 255, 255, 0.4);
+}
+
+.navbar-toggler-icon {
+	background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3e%3cpath stroke='rgba(255, 255, 255, 1)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e");
+}
+
 .admin-badge {
 	color: #38bdf8;
 	font-weight: 700;
